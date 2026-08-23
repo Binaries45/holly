c7810739a6ad194c681148f717a0b6f79a8bf2cf
Binaries <ethan.ramdeen64@gmail.com>
2026-08-23 15:54:18 -0400

    slight rename

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
 const Io = std.Io;
 
-const holy = @import("holy");
-
 pub fn main(init: std.process.Init) !void {
     _ = init;
-    std.debug.print("Hello, World!", .{});
+    std.debug.print("Hello, Holly!", .{});
 }
